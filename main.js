dd2617c6092317751593f1b7de6304fd7ec2c223
Joseph Ely Tuyor <ely.tuyor@yahoo.com>
2023-09-13 10:20:46 +0800

    Print the arrays on the webpage

--- main.js
+++ main.js
@@ -7,31 +7,49 @@ function Book (title, author, numOfPages, readStatus) {
     this.readStatus = readStatus;
 }
 
-Book.prototype.info = function () {
-    return `${this.title} by ${this.author}, ${this.numOfPages} pages, ${this.readStatus}.`
+//Pushes the information to the library
+Book.prototype.add = function () {
+    myLibrary.push(this.title, this.author, this.numOfPages, this.readStatus);
 }
+
 
 function addBooksToLibrary () {
-    //Get user input and print the content on the document
-    //Push every THIS into myLibrary
-    //Return the output
-    myLibrary.push(theHobbit.title, theHobbit.author, theHobbit.numOfPages, theHobbit.readStatus); // Make sure that the objects are connected to the user input later
-}
+    // //Get user input and print the content on the document
+    // //Push every input into myLibrary
+    // const getTitle = prompt("Book Title", '');
+    // const getAuthor = prompt("Author of the Book", '');
+    // const getNumOfPages = prompt('Number of Pages', '');
+    // const getReadStatus = prompt('Read/Not Yet Read', '');
 
-function displayBooks () {
-    //FOR each objects in myLibrary
-    myLibrary.forEach((books) => {
-        //Target DOM element where the objects should be printed
-        const display = document.querySelector(".display-books");
-        //Print the objects with innerText
-        console.log(books);
-        display.innerText = myLibrary;
-    });
+    // myLibrary.push(getTitle, getAuthor, getNumOfPages, getReadStatus);
+    // console.log(myLibrary);
+
+    // console.log(typeof myLibrary === 'object');
+
+    // myLibrary.push(theHobbit.title, theHobbit.author, theHobbit.numOfPages, theHobbit.readStatus);
+
 }
 
 const theHobbit = new Book ("The Hobbit", "J.R.R. Tolkien", "295", "not yet read");
 
 const AtomicHabits = new Book ("Atomic Habits", "James Clear", "288", "finished")
+
+theHobbit.add();
+AtomicHabits.add();
+
+console.log(myLibrary);
+
+function displayBooks () {
+    //FOR each objects in myLibrary
+    for (i = 0; i < myLibrary.length; i++) {
+        console.log(myLibrary[i]);
+        //Target the DOM Element where the library will be printed
+        const displayTitle = document.querySelector(".title");
+        //Print the objects with innerText
+        displayTitle.textContent = myLibrary;
+    }
+    
+}
 
 addBooksToLibrary();
 displayBooks();
